9ffad0bb8465219a7994da8a2ae7ce552aac65c4
Christopher Clark <cclark000@citymail.cuny.edu>
2020-08-23 19:40:23 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 
 
 def turn_count(board)
-  counter = 1
+  counter = 0
   board.each do |token|
     if token != " "
       counter += 1
@@ -12,8 +12,8 @@ end
 
 def current_player(board)
   if turn_count(board) % 2
-    "O"
-  else
     "X"
+  else
+    "O"
   end
 end
